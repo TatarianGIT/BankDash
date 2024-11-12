080b8a524c37e013bcecf1f0a2082f6a47d5c5e1
TatarianGIT <lukipa82@gmail.com>
2024-11-12 04:50:18 +0100

    refactor: move NavBar's data to component

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,19 @@
+import {
+  ChartCandlestick,
+  CreditCard,
+  HandCoins,
+  Handshake,
+  House,
+  PencilRuler,
+  Settings,
+  UserRound,
+  Wrench,
+} from "lucide-react";
+import { ReactNode } from "react";
+
 import { NavLink, useLocation } from "@remix-run/react";
 import { NavLink as MantineNavLink } from "@mantine/core";
 import { cn } from "~/utils/cn.js";
-
-import { NavElementType, navList } from "~/data/NavList";
 
 const NavBar = () => {
   return (
@@ -53,3 +64,67 @@ const NavBarElement = ({ element }: NavBarElementProps) => {
     </div>
   );
 };
+
+export type NavElementType = {
+  id: number;
+  label: string;
+  value: string;
+  icon: ReactNode;
+};
+
+export const navList: NavElementType[] = [
+  {
+    id: 1,
+    label: "Dashboard",
+    value: "/",
+    icon: <House />,
+  },
+  {
+    id: 2,
+    label: "Transactions",
+    value: "transaction",
+    icon: <Handshake />,
+  },
+  {
+    id: 3,
+    label: "Accounts",
+    value: "account",
+    icon: <UserRound />,
+  },
+  {
+    id: 4,
+    label: "Investments",
+    value: "investment",
+    icon: <ChartCandlestick />,
+  },
+  {
+    id: 5,
+    label: "Credit Cards",
+    value: "card",
+    icon: <CreditCard />,
+  },
+  {
+    id: 6,
+    label: "Loans",
+    value: "loan",
+    icon: <HandCoins />,
+  },
+  {
+    id: 7,
+    label: "Servieces",
+    value: "serviece",
+    icon: <PencilRuler />,
+  },
+  {
+    id: 8,
+    label: "My Privileges",
+    value: "privileges",
+    icon: <Wrench />,
+  },
+  {
+    id: 9,
+    label: "Settings",
+    value: "setting",
+    icon: <Settings />,
+  },
+];
