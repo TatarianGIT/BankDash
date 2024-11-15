7d14a362bdb26b5df8b15ca2e4c7faba3b61ba5c
TatarianGIT <lukipa82@gmail.com>
2024-11-15 04:48:57 +0100

    feat: get params from url and pass data to component

--- app/routes/transaction.tsx
+++ app/routes/transaction.tsx
@@ -1,4 +1,4 @@
-import { json } from "@remix-run/node";
+import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import Item from "~/components/common/Item";
 import CreditCard from "~/components/dashboard/CreditCard";
@@ -6,16 +6,41 @@ import MyExpense from "~/components/transaction/MyExpense";
 import RecentTransactionsTable from "~/components/transaction/RecentTransactionsTable";
 import {
   getRecentTransactions,
+  getRecentTransactionsLength,
   RecentTransactionsType,
 } from "~/data/transaction/recentTransations.js";
 import { getMyExpense } from "~/data/transaction/myExpense.js";
 
-export const loader = async () => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
   const myExpense = await getMyExpense();
-  const recentTransactionsData: RecentTransactionsType[] =
-    await getRecentTransactions();
 
-  return json({ myExpense, recentTransactionsData });
+  const url = new URL(request.url);
+
+  const getOperation = url.searchParams.get("operation");
+
+  const operation: RecentTransactionsType["operation"] = getOperation
+    ? ["all", "expense", "income"].includes(getOperation)
+      ? getOperation
+      : "all"
+    : "all";
+
+  const offsetString = url.searchParams.get("offset");
+  const offset = offsetString ? parseInt(offsetString) : 1;
+
+  const limitString = url.searchParams.get("limit");
+  const limit = limitString ? parseInt(limitString) : 5;
+
+  const recentTransactionsLength = await getRecentTransactionsLength({
+    operation,
+  });
+  const recentTransactionsData: RecentTransactionsType[] =
+    await getRecentTransactions({ limit, offset, operation });
+
+  return json({
+    myExpense,
+    recentTransactionsData,
+    recentTransactionsLength,
+  });
 };
 
 const Transaction = () => {
@@ -38,7 +63,10 @@ const Transaction = () => {
       </Item>
 
       <Item leftHeading="Recent Transactions" size="big" className="col-span-1">
-        <RecentTransactionsTable data={data.recentTransactionsData} />
+        <RecentTransactionsTable
+          data={data.recentTransactionsData}
+          dataLength={data.recentTransactionsLength}
+        />
       </Item>
     </>
   );
