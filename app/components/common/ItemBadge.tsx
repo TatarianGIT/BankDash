6e0d7b4613e135ac4daa3a017eccb845f4e4ede7
TatarianGIT <lukipa82@gmail.com>
2024-11-19 04:19:09 +0100

    feat: create `ItemBadge` and `BadgeContent` components

---
+++ app/components/common/ItemBadge.tsx
@@ -0,0 +1,59 @@
+import { Card, Grid, NumberFormatter, Text } from "@mantine/core";
+import { ReactElement, ReactNode } from "react";
+import { cn } from "~/utils/cn.js";
+
+type ItemBadgeProps = {
+  children: ReactElement<typeof BadgeContent>[];
+  className?: string;
+};
+
+const ItemBadge = ({ children, className }: ItemBadgeProps) => {
+  return <Grid className={cn(className, "")}>{children}</Grid>;
+};
+
+type BadgeContentProps = {
+  icon: ReactNode;
+  heading: string;
+  balance: number;
+  colSpan: number;
+  backgroundColor: string;
+};
+
+export const BadgeContent = ({
+  icon,
+  heading,
+  balance,
+  colSpan,
+  backgroundColor,
+}: BadgeContentProps) => {
+  return (
+    <Grid.Col span={colSpan}>
+      <Card
+        withBorder
+        shadow="sm"
+        radius={"lg"}
+        className="flex flex-row gap-2 w-full h-full"
+      >
+        <div
+          className={cn(
+            backgroundColor,
+            "h-12 w-12 rounded-full flex justify-center items-center"
+          )}
+        >
+          {icon}
+        </div>
+        <div className="flex flex-col justify-center">
+          <Text className="text-xs">{heading}</Text>
+          <NumberFormatter
+            className="text-base font-semibold"
+            prefix={"$"}
+            value={balance}
+            thousandSeparator
+          />
+        </div>
+      </Card>
+    </Grid.Col>
+  );
+};
+
+export default ItemBadge;
