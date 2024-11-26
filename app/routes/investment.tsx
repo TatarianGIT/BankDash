99da610766b48b545e0c51a4a0768e26b2110a92
TatarianGIT <lukipa82@gmail.com>
2024-11-26 01:18:30 +0100

    feat: add 3x `ItemBadge` to `investment` route

--- app/routes/investment.tsx
+++ app/routes/investment.tsx
@@ -18,6 +18,33 @@ const Investment = () => {
   });
   return (
     <>
+      <ItemBadgeContainer colSpan={12}>
+        <ItemBadge
+          colSpan={colSpan}
+          icon={<TbMoneybag size={30} className="text-cyan-500" />}
+          backgroundColor="bg-cyan-500/25"
+          heading="Total Invested Amount"
+          balance={150000}
+          type="$"
+        />
+        <ItemBadge
+          colSpan={colSpan}
+          icon={<IoPieChartOutline size={30} className="text-pink-500" />}
+          backgroundColor="bg-pink-500/25"
+          heading="Number of Investments"
+          balance={1250}
+          type="number"
+        />
+        <ItemBadge
+          colSpan={colSpan}
+          icon={<TbRepeat size={30} className="text-blue-500" />}
+          backgroundColor="bg-blue-500/25"
+          heading="Rate of Return"
+          balance={5.81}
+          type="%"
+          operation="income"
+        />
+      </ItemBadgeContainer>
     </>
   );
 };
