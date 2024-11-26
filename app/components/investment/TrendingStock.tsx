1db733e46055a63266af7928b4e4b38d778e6519
TatarianGIT <lukipa82@gmail.com>
2024-11-26 01:20:27 +0100

    feat: add `TrandingStock` table component

---
+++ app/components/investment/TrendingStock.tsx
@@ -0,0 +1,72 @@
+import { NumberFormatter, Table, Text } from "@mantine/core";
+import CardContainer from "../common/CardContainer";
+import { cn } from "~/utils/cn";
+
+const TrendingStock = () => {
+  const rows = elements.map((element) => (
+    <Table.Tr key={element.name}>
+      <Table.Td>
+        <Text className="text-sm">{element.position}</Text>
+      </Table.Td>
+      <Table.Td>
+        <Text className="text-sm">{element.name}</Text>
+      </Table.Td>
+
+      <Table.Td>
+        <Text className="text-sm">
+          <NumberFormatter value={element.price} prefix="$" thousandSeparator />
+        </Text>
+      </Table.Td>
+
+      <Table.Td>
+        <Text className="text-sm">
+          <NumberFormatter
+            className={cn(
+              "",
+              element.type === "income"
+                ? "text-green-400"
+                : element.type === "expense"
+                ? "text-red-400"
+                : ""
+            )}
+            allowNegative={false}
+            value={element.return}
+            prefix={
+              (element.type === "income"
+                ? "+"
+                : element.type === "expense"
+                ? "-"
+                : undefined) + "$"
+            }
+          />
+        </Text>
+      </Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <CardContainer>
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>SL No.</Table.Th>
+            <Table.Th>Name</Table.Th>
+            <Table.Th>Price</Table.Th>
+            <Table.Th>Return</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+    </CardContainer>
+  );
+};
+
+export default TrendingStock;
+
+const elements = [
+  { position: 1, name: "Trivago", price: 520, return: 5, type: "income" },
+  { position: 2, name: "Canon", price: 470, return: 10, type: "income" },
+  { position: 3, name: "Uber Food", price: 305, return: 3, type: "expense" },
+  { position: 4, name: "Nokia", price: 940, return: 2, type: "income" },
+  { position: 5, name: "Tiktok", price: 675, return: 12, type: "expense" },
+];
