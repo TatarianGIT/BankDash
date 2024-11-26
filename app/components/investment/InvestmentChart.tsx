7d1ca447331a33f61309bcc604afe1c93272a185
TatarianGIT <lukipa82@gmail.com>
2024-11-26 01:18:49 +0100

    feat: add `InvestmentChart` component

---
+++ app/components/investment/InvestmentChart.tsx
@@ -0,0 +1,35 @@
+import { LineChart } from "@mantine/charts";
+import { Card } from "@mantine/core";
+import { InvestmentDataType } from "~/data/investment/mockedData";
+
+type InvestmentChartProps = {
+  data: InvestmentDataType[];
+  type: "investment" | "revenue";
+};
+
+const InvestmentChart = ({ type, data }: InvestmentChartProps) => {
+  const color =
+    type === "investment" ? "orange" : type === "revenue" ? "cyan" : undefined;
+  const curveType =
+    type === "investment" ? "linear" : type === "revenue" ? "bump" : undefined;
+
+  return (
+    <Card withBorder shadow="sm" radius={"lg"}>
+      <LineChart
+        className="pt-3 pr-2"
+        h={300}
+        data={data}
+        dataKey="year"
+        series={[{ name: type, color: color }]}
+        curveType={curveType}
+        connectNulls
+        valueFormatter={(value) =>
+          "$" + new Intl.NumberFormat("en-US").format(value)
+        }
+        lineProps={{ isAnimationActive: true }}
+      />
+    </Card>
+  );
+};
+
+export default InvestmentChart;
