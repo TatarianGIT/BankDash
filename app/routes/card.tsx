cf69d02ee54544bd2af3674bf724fcc64e928aee
TatarianGIT <lukipa82@gmail.com>
2024-11-29 12:25:53 +0100

    feat: add new card after successful validation, display `AddNewCard`

--- app/routes/card.tsx
+++ app/routes/card.tsx
@@ -3,6 +3,25 @@ import CardSetting from "~/components/card/CardSetting";
 import ExpenseStatistics from "~/components/card/ExpenseStatistics";
 import CreditCard from "~/components/common/CreditCard";
 import Item from "~/components/common/Item";
+import { ActionFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/react";
+import AddNewCard, { cardFormSchema } from "~/components/card/AddNewCard";
+import { addNewCard } from "~/data/card/addNewCard";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const form = Object.fromEntries(await request.formData());
+
+  const parsed = await cardFormSchema.safeParseAsync(form);
+
+  if (!parsed.success) {
+    const error = parsed.error.format();
+    return json({ error });
+  }
+
+  await addNewCard(form);
+
+  return null;
+};
 
 const Card = () => {
   return (
@@ -18,6 +37,10 @@ const Card = () => {
 
       <Item size="medium" leftHeading="Card List">
         <CardList />
+      </Item>
+
+      <Item size="medium" leftHeading="Add New Card">
+        <AddNewCard />
       </Item>
 
       <Item size="small" leftHeading="Card Setting">
