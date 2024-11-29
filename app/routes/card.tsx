70b272b42754601b995e580a7201fa43eb30240f
TatarianGIT <lukipa82@gmail.com>
2024-11-29 12:19:12 +0100

    feat: display `ExpenseStatistics`

--- app/routes/card.tsx
+++ app/routes/card.tsx
@@ -1,9 +1,15 @@
+import ExpenseStatistics from "~/components/card/ExpenseStatistics";
+
 const Card = () => {
   return (
     <>
       <Item size="full" leftHeading="My Cards">
         <CreditCard variant="primary" />
         <CreditCard variant="alt" />
+      </Item>
+
+      <Item size="small" leftHeading="Card Expense Statistics">
+        <ExpenseStatistics />
       </Item>
     </>
   );
