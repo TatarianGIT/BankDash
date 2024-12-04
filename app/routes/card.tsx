9f5f18e2c62ae22436f887c315f872b482c80386
TatarianGIT <lukipa82@gmail.com>
2024-12-04 18:31:16 +0100

    refactor: update server `action` logic

--- app/routes/card.tsx
+++ app/routes/card.tsx
@@ -1,34 +1,45 @@
+import { ActionFunctionArgs } from "@remix-run/node";
+import { json, useLoaderData } from "@remix-run/react";
+import AddNewCard from "~/components/card/AddNewCard";
 import CardList from "~/components/card/CardList";
 import CardSetting from "~/components/card/CardSetting";
 import ExpenseStatistics from "~/components/card/ExpenseStatistics";
 import CreditCard from "~/components/common/CreditCard";
 import Item from "~/components/common/Item";
-import { ActionFunctionArgs } from "@remix-run/node";
-import { json } from "@remix-run/react";
-import AddNewCard, { cardFormSchema } from "~/components/card/AddNewCard";
-import { addNewCard } from "~/data/card/addNewCard";
+import { addNewCard, CardData, getAllCards } from "~/data/card/addNewCard";
+
+export const loader = async () => {
+  const cards = await getAllCards();
+
+  return json({ cards });
+};
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const form = Object.fromEntries(await request.formData());
+  const form = await request.formData();
 
-  const parsed = await cardFormSchema.safeParseAsync(form);
+  const newCard: CardData = {
+    balance: 0,
+    name: form.get("name") as string,
+    number: form.get("number") as string,
+    type: form.get("type") as "Physical" | "Virtual",
+    date: form.get("date") as string,
+  };
 
-  if (!parsed.success) {
-    const error = parsed.error.format();
-    return json({ error });
-  }
+  const response = await addNewCard(newCard);
 
-  await addNewCard(form);
-
-  return null;
+  return json({ response });
 };
 
 const Card = () => {
+  const { cards } = useLoaderData<typeof loader>();
   return (
     <>
-      <Item size="full" leftHeading="My Cards">
-        <CreditCard variant="primary" />
-        <CreditCard variant="alt" />
+      <Item shouldOverflow={true} size="full" leftHeading="My Cards">
+        {cards.length
+          ? cards.map((creditCard) => (
+              <CreditCard key={creditCard.id} {...creditCard} />
+            ))
+          : null}
       </Item>
 
       <Item size="small" leftHeading="Card Expense Statistics">
