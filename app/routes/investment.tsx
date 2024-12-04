47ba9ce1fb13b7d0322ec11ed7bc93269ee7bd25
TatarianGIT <lukipa82@gmail.com>
2024-12-04 18:45:56 +0100

    refactor: update `ItemBadge` props from `balance` to `description`

--- app/routes/investment.tsx
+++ app/routes/investment.tsx
@@ -31,7 +31,7 @@ const Investment = () => {
           icon={<TbMoneybag size={30} className="text-cyan-500" />}
           backgroundColor="bg-cyan-500/25"
           heading="Total Invested Amount"
-          balance={150000}
+          description={150000}
           type="$"
         />
         <ItemBadge
@@ -39,7 +39,7 @@ const Investment = () => {
           icon={<IoPieChartOutline size={30} className="text-pink-500" />}
           backgroundColor="bg-pink-500/25"
           heading="Number of Investments"
-          balance={1250}
+          description={1250}
           type="number"
         />
         <ItemBadge
@@ -47,7 +47,7 @@ const Investment = () => {
           icon={<TbRepeat size={30} className="text-blue-500" />}
           backgroundColor="bg-blue-500/25"
           heading="Rate of Return"
-          balance={5.81}
+          description={5.81}
           type="%"
           operation="income"
         />
