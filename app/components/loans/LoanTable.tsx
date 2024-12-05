e10fa4fff1c58a0b92287ee934fec795730de25b
TatarianGIT <lukipa82@gmail.com>
2024-12-05 19:54:09 +0100

    feat: add `LoanTable` component

---
+++ app/components/loans/LoanTable.tsx
@@ -0,0 +1,195 @@
+import { Button, Checkbox, NumberFormatter, Table, Text } from "@mantine/core";
+import { useState } from "react";
+import { LoanTableElementType } from "~/data/loan/mockedData";
+import CardContainer from "../common/CardContainer";
+
+type LoanTableProps = {
+  loansData: LoanTableElementType[];
+};
+
+const LoanTable = ({ loansData }: LoanTableProps) => {
+  return (
+    <CardContainer>
+      <DataTable loansData={loansData} />
+    </CardContainer>
+  );
+};
+
+type DataTableProps = {
+  loansData: LoanTableElementType[];
+};
+
+const DataTable = ({ loansData }: DataTableProps) => {
+  const [selectedRows, setSelectedRows] = useState<LoanTableElementType[]>([]);
+
+  const selectedMoneyTotal = selectedRows.reduce(
+    (accumulator, loan) => accumulator + loan.money,
+    0
+  );
+
+  const selectedMoneyLeftTotal = selectedRows.reduce(
+    (accumulator, loan) => accumulator + loan.moneyLeft,
+    0
+  );
+
+  const selectedInstallmentTotal = selectedRows.reduce(
+    (accumulator, loan) => accumulator + Number(loan.installment),
+    0
+  );
+
+  const totalMoney = loansData.reduce(
+    (accumulator, loan) => accumulator + Number(loan.money),
+    0
+  );
+
+  const totalMoneyLeft = loansData.reduce(
+    (accumulator, loan) => accumulator + Number(loan.moneyLeft),
+    0
+  );
+
+  const totalInstallment = loansData.reduce(
+    (accumulator, loan) => accumulator + Number(loan.installment),
+    0
+  );
+
+  const header = (
+    <Table.Tr>
+      <Table.Th>
+        <Checkbox
+          checked={JSON.stringify(selectedRows) == JSON.stringify(loansData)}
+          variant="outline"
+          aria-label="Select row"
+          onChange={(event) =>
+            setSelectedRows(event.currentTarget.checked ? [...loansData] : [])
+          }
+        />
+      </Table.Th>
+      <Table.Th className="md:table-cell hidden">SL No</Table.Th>
+      <Table.Th>Loan Money</Table.Th>
+      <Table.Th>Left to repay</Table.Th>
+      <Table.Th className="md:table-cell hidden">Duration</Table.Th>
+      <Table.Th className="md:table-cell hidden">Intrest rate</Table.Th>
+      <Table.Th className="sm:table-cell hidden">Installment</Table.Th>
+      <Table.Th>Repay</Table.Th>
+    </Table.Tr>
+  );
+
+  const footer = (
+    <Table.Tr className="border-t-4 ">
+      <Table.Th></Table.Th>
+      <Table.Th>Total</Table.Th>
+      <Table.Th>
+        <FormatedNumber value={totalMoney} />
+      </Table.Th>
+      <Table.Th>
+        <FormatedNumber value={totalMoneyLeft} />
+      </Table.Th>
+      <Table.Th className="md:table-cell hidden"></Table.Th>
+      <Table.Th className="md:table-cell hidden"></Table.Th>
+      <Table.Th className="sm:table-cell hidden">
+        <FormatedNumber value={totalInstallment} /> {" / month"}
+      </Table.Th>
+      <Table.Th></Table.Th>
+    </Table.Tr>
+  );
+
+  const selectedTotal = (
+    <Table.Tr>
+      <Table.Th></Table.Th>
+      <Table.Th>Selected</Table.Th>
+      <Table.Th>
+        <FormatedNumber value={selectedMoneyTotal.toFixed(2)} />
+      </Table.Th>
+      <Table.Th>
+        <FormatedNumber value={selectedMoneyLeftTotal.toFixed(2)} />
+      </Table.Th>
+      <Table.Th className="md:table-cell hidden"></Table.Th>
+      <Table.Th className="md:table-cell hidden"></Table.Th>
+      <Table.Th className="sm:table-cell hidden">
+        <FormatedNumber value={selectedInstallmentTotal.toFixed(2)} />{" "}
+        {" / month"}
+      </Table.Th>
+      <Table.Th></Table.Th>
+    </Table.Tr>
+  );
+
+  const rows = loansData.map((loan) => {
+    const isSelected = selectedRows.some((item) => item.SLNo === loan.SLNo);
+
+    return (
+      <Table.Tr
+        key={loan.SLNo}
+        bg={isSelected ? "var(--mantine-color-blue-light)" : undefined}
+      >
+        <Table.Td>
+          <Checkbox
+            aria-label="Select row"
+            checked={isSelected}
+            onChange={(event) =>
+              setSelectedRows(
+                event.currentTarget.checked
+                  ? [...selectedRows, loan].sort()
+                  : selectedRows
+                      .filter((oldLoan) => oldLoan.SLNo !== loan.SLNo)
+                      .sort()
+              )
+            }
+          />
+        </Table.Td>
+
+        <Table.Td className="md:table-cell hidden">#{loan.SLNo}</Table.Td>
+        <Table.Td>
+          <FormatedNumber value={loan.money} />
+        </Table.Td>
+        <Table.Td>
+          <FormatedNumber value={loan.moneyLeft} />
+        </Table.Td>
+        <Table.Td className="md:table-cell hidden">
+          {loan.duration} months
+        </Table.Td>
+        <Table.Td className="md:table-cell hidden">{loan.rate}%</Table.Td>
+        <Table.Td className="sm:table-cell hidden">
+          <FormatedNumber value={loan.installment} /> {" / month"}
+        </Table.Td>
+
+        <Table.Td>
+          <RepayButton />
+        </Table.Td>
+      </Table.Tr>
+    );
+  });
+
+  return (
+    <>
+      <Table withTableBorder>
+        <Table.Thead>{header}</Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+        <Table.Tfoot>
+          {footer}
+          {selectedTotal}
+        </Table.Tfoot>
+      </Table>
+    </>
+  );
+};
+
+const RepayButton = () => {
+  return (
+    <Button variant="outline" className="rounded-full">
+      Repay
+    </Button>
+  );
+};
+
+const FormatedNumber = ({ value }: { value: number | string }) => {
+  return (
+    <NumberFormatter
+      thousandSeparator
+      prefix="$"
+      value={Number(value).toFixed(2)}
+    />
+  );
+};
+
+export default LoanTable;
+
