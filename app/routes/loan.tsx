d11629b0c5c8b1cbc2fbd1dcca23acc16c161fcf
TatarianGIT <lukipa82@gmail.com>
2024-12-05 19:54:42 +0100

    feat: get mocked data and pass it to the `LoanTable`

--- app/routes/loan.tsx
+++ app/routes/loan.tsx
@@ -1,9 +1,25 @@
+import { json, useLoaderData } from "@remix-run/react";
+import Item from "~/components/common/Item";
 import LoanBadgeSection from "~/components/loans/LoanBadgeSection";
+import LoanTable from "~/components/loans/LoanTable";
+import { getLoansData } from "~/data/loan/mockedData";
+
+export const loader = async () => {
+  const loansData = await getLoansData();
+
+  return json({ loansData });
+};
 
 const Loan = () => {
+  const { loansData } = useLoaderData<typeof loader>();
+
   return (
     <>
       <LoanBadgeSection />
+
+      <Item leftHeading="Active Loans Overview" size="full">
+        <LoanTable loansData={loansData} />
+      </Item>
     </>
   );
 };
