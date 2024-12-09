6af49dd1592747ec28c4308646644eb87157139a
TatarianGIT <lukipa82@gmail.com>
2024-12-09 16:59:10 +0100

    fix: fix hydration error

--- app/data/common/creditCard.ts
+++ app/data/common/creditCard.ts
@@ -2,16 +2,16 @@ import { CardData } from "../card/addNewCard";
 
 export const MockedCreditCardData: CardData[] = [
   {
-    id: Date.now().toString(),
-    balance: Math.floor(Math.random() * 10000),
+    id: Date.now().toString() + Math.ceil(Math.random() * 10000),
+    balance: 21042,
     date: "07/24",
     name: "John Smith",
     number: "5225 1025 5911 1029",
     type: "Physical",
   },
   {
-    id: Date.now().toString(),
-    balance: Math.floor(Math.random() * 10000),
+    id: Date.now().toString() + Math.ceil(Math.random() * 10000),
+    balance: 7288,
     date: "02/25",
     name: "Emma Smith",
     number: "1255 2825 8892 5826",
