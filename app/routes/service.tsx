c2ab1c40b774598e650ccbf8625e77fd22d0bf90
TatarianGIT <lukipa82@gmail.com>
2024-12-10 18:45:29 +0100

    feat: display `BankServiceList`

--- app/routes/service.tsx
+++ app/routes/service.tsx
@@ -1,9 +1,15 @@
+import Item from "~/components/common/Item";
+import BankServiceList from "~/components/service/BankServiceList";
 import ServiceBadgeSection from "~/components/service/ServiceBadgeSection";
 
 const Service = () => {
   return (
     <>
       <ServiceBadgeSection />
+
+      <Item leftHeading="Back Services List" size="full">
+        <BankServiceList />
+      </Item>
     </>
   );
 };
