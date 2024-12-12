0a66056b1a55945f53ec1ff1b3e35f756bbafcbd
TatarianGIT <lukipa82@gmail.com>
2024-12-12 19:20:43 +0100

    feat: add `SecurityTab`

---
+++ app/components/setting/SecurityTab.tsx
@@ -0,0 +1,35 @@
+import { Text } from "@mantine/core";
+import SwitchOption from "./SwitchOption";
+import FormInput from "./FormInput";
+import SaveButton from "./SaveButton";
+
+const SecurityTab = () => {
+  return (
+    <div className="pt-10 px-1 md:px-6 flex flex-col gap-4">
+      <div className="flex flex-col gap-3">
+        <Text>Two-factor Authentication</Text>
+        <SwitchOption label="Enable or disable two factor authentication" />
+      </div>
+
+      <div className="w-1/2 flex flex-col gap-3">
+        <Text>Change Password</Text>
+        <FormInput
+          type="password"
+          placeholder="**********"
+          label="Current Password"
+        />
+        <FormInput
+          type="password"
+          placeholder="**********"
+          label="New Password"
+        />
+      </div>
+
+      <div className="flex justify-end">
+        <SaveButton />
+      </div>
+    </div>
+  );
+};
+
+export default SecurityTab;
