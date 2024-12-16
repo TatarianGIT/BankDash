d4cfe27a6e68aace22bcd6c2f834967bffe679d9
TatarianGIT <lukipa82@gmail.com>
2024-12-16 21:33:04 +0100

    feat: make `SwtichOption` controlled input

--- app/components/setting/SwitchOption.tsx
+++ app/components/setting/SwitchOption.tsx
@@ -1,7 +1,22 @@
 import { Switch } from "@mantine/core";
+import { useState } from "react";
 
-const SwitchOption = ({ label }: { label: string }) => {
-  return <Switch label={label} />;
+const SwitchOption = ({
+  label,
+  checked,
+}: {
+  label: string;
+  checked: boolean;
+}) => {
+  const [isChecked, setIsChecked] = useState<boolean>(checked);
+
+  return (
+    <Switch
+      label={label}
+      checked={isChecked}
+      onChange={(e) => setIsChecked(e.currentTarget.checked)}
+    />
+  );
 };
 
 export default SwitchOption;
