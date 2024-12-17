7343525cd6afc6ade77620713cde0343e6e671b0
TatarianGIT <lukipa82@gmail.com>
2024-12-17 17:37:58 +0100

    feat: add `LoadingItem` component

---
+++ app/components/common/LoadingItem.tsx
@@ -0,0 +1,41 @@
+import { Skeleton } from "@mantine/core";
+import { Loader2Icon } from "lucide-react";
+import CardContainer from "./CardContainer";
+import { ReactNode, Suspense } from "react";
+import { Await } from "@remix-run/react";
+import { cn } from "~/utils/cn";
+
+type LoadingItemProps<T> = {
+  data: Promise<T> | undefined;
+  children: (resolvedData: Awaited<T>) => ReactNode | ReactNode[];
+  className?: string;
+  fallback?: ReactNode | undefined;
+};
+
+const LoadingItem = <T,>({
+  data,
+  children,
+  className,
+  fallback,
+}: LoadingItemProps<T>) => {
+  return (
+    <Suspense
+      fallback={fallback ?? <SkeletonContainer className={className} />}
+    >
+      <Await resolve={data}>{(response) => children(response!)}</Await>
+    </Suspense>
+  );
+};
+
+export const SkeletonContainer = ({ className }: { className?: string }) => {
+  return (
+    <CardContainer className={cn("p-0 w-full h-80 relative", className)}>
+      <Skeleton className="w-full h-full" />
+      <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+        <Loader2Icon className="animate-spin" />
+      </div>
+    </CardContainer>
+  );
+};
+
+export default LoadingItem;
