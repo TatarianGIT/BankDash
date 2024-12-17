53566e2354ac167875e22c5c891551d67bcb7ed2
TatarianGIT <lukipa82@gmail.com>
2024-12-17 18:57:57 +0100

    feat: add loading skeleton to `SwtichOption` component

--- app/components/setting/SwitchOption.tsx
+++ app/components/setting/SwitchOption.tsx
@@ -1,14 +1,22 @@
-import { Switch } from "@mantine/core";
+import { Skeleton, Switch } from "@mantine/core";
 import { useState } from "react";
+import { WithLoading } from "~/types";
 
-const SwitchOption = ({
-  label,
-  checked,
-}: {
+type SwitchOptionsProps = WithLoading<{
   label: string;
   checked: boolean | undefined;
-}) => {
+}>;
+
+const SwitchOption = ({ label, checked, isLoading }: SwitchOptionsProps) => {
   const [isChecked, setIsChecked] = useState<boolean | undefined>(checked);
+
+  if (isLoading)
+    return (
+      <div className="flex gap-2">
+        <Skeleton className="w-10 rounded-3xl" />
+        {label}
+      </div>
+    );
 
   return (
     <Switch
