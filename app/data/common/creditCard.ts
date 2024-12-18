8234ad57884a7d3d06142aa9aaed8cd49d4e8ea0
TatarianGIT <lukipa82@gmail.com>
2024-12-18 16:21:47 +0100

    feat: add common card method to get specific amout of cards

--- app/data/common/creditCard.ts
+++ app/data/common/creditCard.ts
@@ -1,20 +1,7 @@
-import { CardData } from "../card/addNewCard";
+import { wait } from "~/utils/wait";
+import { card } from "../card/addNewCard";
 
-export const MockedCreditCardData: CardData[] = [
-  {
-    id: Date.now().toString() + Math.ceil(Math.random() * 10000),
-    balance: 21042,
-    date: "07/24",
-    name: "John Smith",
-    number: "5225 1025 5911 1029",
-    type: "Physical",
-  },
-  {
-    id: Date.now().toString() + Math.ceil(Math.random() * 10000),
-    balance: 7288,
-    date: "02/25",
-    name: "Emma Smith",
-    number: "1255 2825 8892 5826",
-    type: "Virtual",
-  },
-];
+export const getCard = async (amount: number) => {
+  await wait(800);
+  return card.getAmount(amount);
+};
