06299e3bbdadf0c15027717b88d26ec4f26606e9
TatarianGIT <lukipa82@gmail.com>
2025-01-15 16:02:40 +0100

    refactor: add loading state to `SaveButton` and organize logic

--- app/components/setting/SaveButton.tsx
+++ app/components/setting/SaveButton.tsx
@@ -1,7 +1,28 @@
-import { Button } from "@mantine/core";
+import { Button as MantineButton } from "@mantine/core";
 
-const SaveButton = () => {
-  return <Button className="px-10 mt-6 mb-2">Save</Button>;
+const SaveButton = ({ isLoading = false }: { isLoading?: boolean }) => {
+  if (isLoading) return <Button label="Save" isDisabled={true} />;
+
+  return <Button label="Save" />;
+};
+
+const Button = ({
+  label,
+  isDisabled = false,
+}: {
+  label: string;
+  isDisabled?: boolean;
+}) => {
+  return (
+    <MantineButton
+      variant="filled"
+      disabled={isDisabled}
+      type="submit"
+      className="px-10 mt-6 mb-2"
+    >
+      {label}
+    </MantineButton>
+  );
 };
 
 export default SaveButton;
