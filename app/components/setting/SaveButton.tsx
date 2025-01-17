5aabf9f5ddadc25734fbd95b2634f6978cd414c3
TatarianGIT <lukipa82@gmail.com>
2025-01-17 16:41:39 +0100

    refactor: update `SaveButton`

--- app/components/setting/SaveButton.tsx
+++ app/components/setting/SaveButton.tsx
@@ -1,26 +1,46 @@
 import { Button as MantineButton } from "@mantine/core";
+import { LoaderCircle } from "lucide-react";
+import { ReactNode } from "react";
+import { cn } from "~/utils/cn";
 
-const SaveButton = ({ isLoading = false }: { isLoading?: boolean }) => {
-  if (isLoading) return <Button label="Save" isDisabled={true} />;
+type SaveButtonProps = {
+  isLoading?: boolean;
+  isSubmitting?: boolean;
+};
 
-  return <Button label="Save" />;
+const SaveButton = ({
+  isLoading = false,
+  isSubmitting = false,
+}: SaveButtonProps) => {
+  if (isLoading) return <Button isDisabled={true}>Save</Button>;
+
+  if (isSubmitting)
+    return (
+      <Button className="px-6">
+        Saving <LoaderCircle className="ml-2 animate-spin w-4 h-4" />
+      </Button>
+    );
+
+  return <Button> Save</Button>;
 };
 
 const Button = ({
-  label,
+  children,
+  className,
   isDisabled = false,
 }: {
-  label: string;
+  children: ReactNode;
   isDisabled?: boolean;
+  className?: string;
 }) => {
   return (
     <MantineButton
       variant="filled"
       disabled={isDisabled}
       type="submit"
-      className="px-10 mt-6 mb-2"
+      className={cn("px-10 mt-6 mb-2", className)}
     >
-      {label}
+      {children}
     </MantineButton>
   );
 };
