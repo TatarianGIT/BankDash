a4da2cd83ec2422da32189174a6e7a58c683da2f
TatarianGIT <lukipa82@gmail.com>
2025-01-17 19:52:44 +0100

    feat: add `useNotification` hook

---
+++ app/hooks/useNotification.tsx
@@ -0,0 +1,47 @@
+import { notifications } from "@mantine/notifications";
+import { FetcherWithComponents } from "@remix-run/react";
+import { useEffect } from "react";
+import { SettingResponse } from "~/data/setting/mockedData";
+
+type UseNotificationProps = {
+  id: string;
+  fetcher: FetcherWithComponents<SettingResponse>;
+};
+
+const useNotification = ({ fetcher, id }: UseNotificationProps) => {
+  useEffect(() => {
+    if (fetcher.state === "submitting") {
+      notifications.show({
+        id,
+        loading: true,
+        color: "blue",
+        title: "Loading...",
+        message: "This may take a while.",
+      });
+    }
+
+    if (fetcher.data?.status === "error") {
+      notifications.show({
+        id,
+        color: "red",
+        title: "Error!",
+        message: `An error occurred: ${fetcher.data.message}.`,
+      });
+    }
+
+    if (fetcher.data?.status === "success") {
+      notifications.show({
+        id,
+        color: "green",
+        title: "Success!",
+        message: `Operation completed. \n${fetcher.data.message}.`,
+      });
+    }
+
+    return () => {
+      notifications.hide(id);
+    };
+  }, [fetcher, id]);
+};
+
+export default useNotification;
