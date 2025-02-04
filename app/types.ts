253a47e674d944fc4f42809b53459d42e1c802d6
TatarianGIT <lukipa82@gmail.com>
2025-02-04 03:25:11 +0100

    feat: add notification response type

--- app/types.ts
+++ app/types.ts
@@ -5,3 +5,8 @@ export type WithLoading<T> =
   | ({ isLoading: true } & Partial<T>);
 
 export type Override<T, R> = Omit<T, keyof R> & R;
+
+export type NotificationResponse = {
+  status: "success" | "error";
+  message: string;
+};
