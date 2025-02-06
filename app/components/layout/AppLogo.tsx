c01beb1e9ddc2035b3930062f2d1729b5fd848a8
TatarianGIT <lukipa82@gmail.com>
2025-02-06 04:45:39 +0100

    feat: redirect to root page after logo click

--- app/components/layout/AppLogo.tsx
+++ app/components/layout/AppLogo.tsx
@@ -1,11 +1,15 @@
+import { Link } from "@remix-run/react";
 import { Feather } from "lucide-react";
 
 const AppLogo = () => {
   return (
-    <div className="flex w-full absolute -z-10 justify-center items-center gap-2 text-lg font-bold">
+    <Link
+      to={"/"}
+      className="flex w-full justify-center items-center gap-2 text-lg font-bold"
+    >
       <Feather size={30} />
       <span>BankDash.</span>
-    </div>
+    </Link>
   );
 };
 
