2cc097fc9d589d99c2f94278721cd61fcba9582f
TatarianGIT <lukipa82@gmail.com>
2025-02-17 16:51:18 +0100

    refactor: remove usless code

--- app/routes/setting._index.tsx
+++ app/routes/setting._index.tsx
@@ -3,9 +3,3 @@ import { redirect } from "@remix-run/react";
 export const loader = async () => {
   return redirect("/setting/profile");
 };
-
-const Setting = () => {
-  return null;
-};
-
-export default Setting;
