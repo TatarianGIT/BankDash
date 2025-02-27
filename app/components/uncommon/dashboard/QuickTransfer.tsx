9bce9caacee44857341cb62cb7da40d841bdfd60
TatarianGIT <lukipa82@gmail.com>
2025-02-28 00:17:32 +0100

    refactor: make `QuickTransfer` prettier

--- app/components/uncommon/dashboard/QuickTransfer.tsx
+++ app/components/uncommon/dashboard/QuickTransfer.tsx
@@ -10,11 +10,12 @@ import {
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useFetcher } from "@remix-run/react";
-import { Loader, Send } from "lucide-react";
+import { Banknote, Loader, Send } from "lucide-react";
 import { useEffect, useState } from "react";
 import { UserData } from "~/data/dashboard/contacts.js";
 import useNotification from "~/hooks/useNotification";
 import { action } from "~/routes/dashboard";
+import { cn } from "~/utils/cn";
 
 type QuickTransferProps = {
   data: UserData[];
@@ -53,21 +54,65 @@ const QuickTransfer = ({ data }: QuickTransferProps) => {
     <div className="p-0 flex-col w-full">
       {data.length ? (
         <>
-          <Modal opened={opened} onClose={close} title={"Confirm action"}>
+          <Modal
+            opened={opened}
+            onClose={close}
+            title={
+              <Text size="xl" fw={600}>
+                Confirm Transfer
+              </Text>
+            }
+            radius="lg"
+            padding="xl"
+            centered
+            role="alertdialog"
+          >
             <fetcher.Form
               onSubmit={() => setShouldClear(true)}
               method="POST"
-              className="flex flex-col gap-4"
+              className="flex flex-col gap-6"
             >
+              <div className="flex justify-evenly  items-center h-24 bg-mantineColorGray1 dark:bg-mantineColorDark6 rounded-2xl">
+                <div className="w-16 h-16 bg-mantineColorDark1 dark:bg-mantineColorDark5 rounded-full flex justify-center items-center">
+                  <Banknote className="w-full h-full text-white p-2" />
+                </div>
+                <div className="flex gap-2 sm:gap-4">
+                  <div className="w-2 h-2 sm:w-3 sm:h-3 bg-mantineColorBlue5 dark:bg-mantineColorBlue8 rounded-full animate-[bounce_1.5s_infinite_0ms]" />
+                  <div className="w-2 h-2 sm:w-3 sm:h-3 bg-mantineColorBlue5 dark:bg-mantineColorBlue8 rounded-full animate-[bounce_1.5s_infinite_150ms]" />
+                  <div className="w-2 h-2 sm:w-3 sm:h-3 bg-mantineColorBlue5 dark:bg-mantineColorBlue8 rounded-full animate-[bounce_1.5s_infinite_300ms]" />
+                </div>
+
+                <div className="w-16 h-16">
+                  <Avatar
+                    src={selectedContact?.avatar}
+                    className="w-full h-full"
+                  />
+                </div>
+              </div>
+
               <Text size="lg">
-                {`Are you sure, that You want to send `}
-                {formatedValue}
-                {` to ${selectedContact?.firstName} ${selectedContact?.lastName}?`}
+                You are about to send{" "}
+                <span className="text-green-600 font-semibold text-lg">
+                  {formatedValue}
+                </span>{" "}
+                to{" "}
+                <span className="text-cyan-600 font-semibold text-lg">
+                  {selectedContact?.firstName} {selectedContact?.lastName}.
+                </span>
               </Text>
-              <div className="flex justify-between w-full">
-                <Button color="red" onClick={close} size="md">
+              <Text size="lg">This action cannot be undone.</Text>
+
+              <div className="flex flex-col sm:flex-row gap-3 justify-between mt-4">
+                <Button
+                  onClick={close}
+                  size="md"
+                  variant="light"
+                  color="gray"
+                  className="max-sm:order-1"
+                >
                   Cancel
                 </Button>
+
                 <Button
                   type="submit"
                   size="md"
@@ -101,6 +146,9 @@ const QuickTransfer = ({ data }: QuickTransferProps) => {
                 paddingBottom: "0.2rem",
               },
               root: { padding: "1rem" },
+              control: {
+                margin: "-25px",
+              },
             }}
             initialSlide={0}
             onSlideChange={(index) => setSelectedIndex(index)}
@@ -152,6 +200,8 @@ type AmountInputProps = {
 };
 
 const AmountInput = ({ open, setValue, value }: AmountInputProps) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
   return (
     <div className="mt-auto w-full p-4">
       <div className="flex gap-4 flex-wrap items-center">
@@ -159,14 +209,26 @@ const AmountInput = ({ open, setValue, value }: AmountInputProps) => {
         <div className="relative min-w-[250px] flex-1">
           <NumberInput
             placeholder="Amount..."
+            thousandSeparator=","
+            prefix="$ "
+            allowNegative={false}
+            min={0}
+            max={100_000_000}
+            allowDecimal={true}
+            decimalScale={2}
             radius={"xl"}
             value={value}
             onChange={(newValue) => setValue(Number(newValue))}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
           />
           <Button
             disabled={value <= 0}
             onClick={open}
-            className="rounded-full flex justify-center items-center absolute bottom-0 right-0 z-10 data-[disabled]:dark:bg-gray-700 data-[disabled]:cursor-not-allowed"
+            className={cn(
+              "rounded-full flex justify-center items-center absolute bottom-0 right-0 z-10 data-[disabled]:dark:bg-gray-700 data-[disabled]:cursor-not-allowed",
+              isFocused && "border-2 border-mantinePrimaryColor8"
+            )}
             type="submit"
           >
             <Text>Send</Text>
