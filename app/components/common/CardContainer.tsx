b5b2f2616fec0e341dfcbc8585951131eb5a7308
TatarianGIT <lukipa82@gmail.com>
2025-03-08 02:14:38 +0100

    feat: spread rest of the props

--- app/components/common/CardContainer.tsx
+++ app/components/common/CardContainer.tsx
@@ -1,14 +1,18 @@
-import { Card } from "@mantine/core";
+import { Card, CardProps } from "@mantine/core";
 import { ReactNode } from "react";
 
 type CardContainerProps = {
   children: ReactNode;
   className?: string;
-};
+} & CardProps;
 
-const CardContainer = ({ children, className }: CardContainerProps) => {
+const CardContainer = ({
+  children,
+  className,
+  ...rest
+}: CardContainerProps) => {
   return (
-    <Card radius={"lg"} withBorder shadow="sm" className={className}>
+    <Card radius={"lg"} withBorder shadow="sm" className={className} {...rest}>
       {children}
     </Card>
   );
